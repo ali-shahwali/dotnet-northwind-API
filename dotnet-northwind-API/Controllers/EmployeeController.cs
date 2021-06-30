fe38860518ed5f260cebc64ce44d19fcdd385593
Ali Shahwali <ali_zinq@hotmail.com>
2021-06-30 15:06:55 +0200

    Add project files.

---
+++ dotnet-northwind-API/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_northwind_API.Models;
+using dotnet_northwind_API.Data;
+
+namespace dotnet_northwind_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private ApplicationDbContext database;
+
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            database = context;
+        }
+
+        [HttpGet("getAll")]
+        public List<Employee> Get()
+        {
+            return database.Employees.ToList();
+        }
+
+        [HttpGet("getAllNames")]
+        public List<string> GetNames()
+        {
+            List<string> names = new List<string>();
+            List<Employee> employees = database.Employees.ToList();
+            foreach (Employee employee in employees)
+            {
+                names.Add(employee.FirstName + " " + employee.LastName);
+            }
+
+            return names;
+        }
+
+        [HttpGet("getEmployeeById/{employeeId}")]
+        public Employee GetEmployeeById(int employeeId)
+        {
+            return database.Employees.Where(x => x.EmployeeId == employeeId).FirstOrDefault();
+        }
+    }
+}
---
+++ dotnet-northwind-API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using dotnet_northwind_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_northwind_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
